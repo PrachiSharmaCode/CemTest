b60f9bad107917c68afd381f895730c1ec3fbdb1
PrachiMobileApp <prachi.mobileapp@gmail.com>
2020-10-13 20:08:02 -0700

    Updates

---
+++ precache-manifest.27adcd36c7bed477d1b88973420366d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d24f1822266b3e5c16f60f9273b6b15",
+    "url": "/PrachiSharmaCode/CemTest/index.html"
+  },
+  {
+    "revision": "33b356f34adaefe4e6c9",
+    "url": "/PrachiSharmaCode/CemTest/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2d708def87fcf2d0c59f",
+    "url": "/PrachiSharmaCode/CemTest/static/css/main.e0147863.chunk.css"
+  },
+  {
+    "revision": "33b356f34adaefe4e6c9",
+    "url": "/PrachiSharmaCode/CemTest/static/js/2.ed23f824.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/PrachiSharmaCode/CemTest/static/js/2.ed23f824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d708def87fcf2d0c59f",
+    "url": "/PrachiSharmaCode/CemTest/static/js/main.aae7d0ab.chunk.js"
+  },
+  {
+    "revision": "fb288a07be4c697116fa",
+    "url": "/PrachiSharmaCode/CemTest/static/js/runtime-main.3eef7d7c.js"
+  },
+  {
+    "revision": "4c63f66c8c90924555cb9f5027f40f05",
+    "url": "/PrachiSharmaCode/CemTest/static/media/cemLogo.4c63f66c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PrachiSharmaCode/CemTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
